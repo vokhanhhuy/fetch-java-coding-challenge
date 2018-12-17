efc6b20027c61b2e838915bb94dde220b07b1d48
Huy <huyvo@kms-technology.com>
2018-12-17 22:58:49 +0700

    Initialize project: Java Coding Challenge

---
+++ src/main/java/com/fetch/technology/javacodingchallenge/JavaCodingChallengeApplication.java
@@ -0,0 +1,14 @@
+package com.fetch.technology.javacodingchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodingChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCodingChallengeApplication.class, args);
+	}
+
+}
+
